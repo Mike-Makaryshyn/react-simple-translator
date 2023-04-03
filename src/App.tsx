5ab1544589be7f23aeda20987f183ebdb2db8c57
Michael Makaryshyn <mi5hqaaaaov@gmail.com>
2023-04-03 17:18:11 +0300

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Field from "./components/Filed";
+import Translate from "./components/Translate";
+import Languages from "./components/Languages";
+
+export default function App() {
+  return (
+    <div>
+      <h3>Hi there!</h3>
+    </div>
+  );
+}
+
---
+++ src/components/Filed.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Flower from './svg/Flower';
+
+interface FlowerArgs {
+   value: string,
+   onChange: (value: string) => void;
+}
+
+function Field({ value, onChange }: FlowerArgs) {
+  return (
+    <div className="field">
+      <Flower className="flower-right" fill="rgb(110, 146, 119)" />
+      <Flower className="flower-left" fill="rgb(249, 148, 59)" />
+      <h1>Translate App</h1>
+      <label>Enter English</label>
+      <input
+        className="input"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default Field;
